fa1813242e85de0fadf85501d01cf59139f3bb3f
Andreas Dorta <eichi@gmx.ch>
2025-06-27 00:02:51 +0200

    Separate classes for classic mandelbrot set and mandelbrot cross trap

--- app.py
+++ app.py
@@ -1,44 +1,63 @@
 # mandelbrot_app.py
 
 import streamlit as st
+import matplotlib.pyplot as plt
+from enum import Enum
+
 from fractalart.core import *
 from fractalart.fractal import *
-import matplotlib.pyplot as plt
 
-# Streamlit interface
-st.title("Mandelbrot-Explorer")
-st.sidebar.header("Parameters")
+# --- Enum Definition ---
+class FractalType(Enum):
+    MANDELBROT = "Mandelbrot Set"
+    JULIA = "Julia Set"
 
-res = st.sidebar.slider("resolutiomn", 100, 8000, 800, step=100)
-max_iter = st.sidebar.slider("max. iterations", 100, 3000, 1000, step=100)
-st.sidebar.text("center position")
-c_x = st.sidebar.number_input("x-coordinate", format="%0.10f", value = -0.5)
-c_y = st.sidebar.number_input("y-coordinate", format="%0.10f", value = 0.0)
-zoom = st.sidebar.select_slider("Zoom factor", options = [1, 5, 25, 125, 625, 3125, 15625, 78125], value = 1.0)
-hist_eq_on = st.sidebar.toggle("Histogram eualization", value=True)
+# --- Factory Function ---
+def create_fractal(fractal_type: FractalType) -> Fractal:
+    fractal_classes = {
+        FractalType.MANDELBROT: Mandelbrot,
+        FractalType.JULIA: Julia,
+    }
+    return fractal_classes[fractal_type]()
 
+# --- Sidebar Controls ---
+with st.sidebar:
+    st.header("Parameters")
 
-# Parameters
-width, height = res, res
+    selected_type = st.selectbox(
+        "Fractal type",
+        options=list(FractalType),
+        format_func=lambda ft: ft.value
+    )
+
+    resolution = st.slider("Resolution", 100, 4000, 600, step=100)
+    # max_iterations = st.slider("Max iterations", 100, 3000, 1000, step=100)
+    max_iterations = st.select_slider("Max iterations", options=[10, 100, 200, 500, 1000, 3000, 5000, 10000], value=200)
+
+    st.text("Center position")
+    center_x = st.number_input("X-coordinate", format="%.10f", value=-0.5)
+    center_y = st.number_input("Y-coordinate", format="%.10f", value=0.0)
+
+    zoom = st.select_slider("Zoom factor", options=[1, 5, 25, 125, 625, 3125, 15625, 78125], value=1)
+    hist_eq_enabled = st.toggle("Histogram equalization", value=True)
+
+# --- Fractal Configuration ---
+fractal = create_fractal(selected_type)
+fractal.resolution = (resolution, resolution)
+fractal.max_iter = max_iterations
+fractal.set_zoom(zoom, (center_x, center_y))
+fractal.render()
+
+# --- Histogram Equalization ---
+if hist_eq_enabled:
+    fractal.equalize_histogram()
+
+# --- Plotting ---
 delta = 2 / zoom
-center = c_x, c_y
-x_min, y_min = center[0] - delta, center[1] - delta
-x_max, y_max = center[0] + delta, center[1] + delta
+x_min, x_max = center_x - delta, center_x + delta
+y_min, y_max = center_y - delta, center_y + delta
 
-# Render the fractal
-m = Mandelbrot()
-m.resolution = res, res
-m.set_zoom(zoom, (c_x, c_y))
-m.render()
-
-# Equalize smooth values
-if hist_eq_on:
-    m.equalize_histogram()
-
-# Plotting
-plt.figure()
-plt.imshow(m._image, extent=(x_min, x_max, y_min, y_max), cmap='turbo')
-plt.axis('off')
-st.pyplot(plt)
-
-
+fig, ax = plt.subplots()
+ax.imshow(fractal._image, extent=(x_min, x_max, y_min, y_max), cmap='turbo')
+ax.axis("off")
+st.pyplot(fig)
